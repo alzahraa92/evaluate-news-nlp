cf4cb0b3065ec6bab21794d78a5f312a4852199b
alzahraa92 <77387933+alzahraa92@users.noreply.github.com>
2021-04-23 01:21:40 +0200

    confirm

--- src/client/index.js
+++ src/client/index.js
@@ -6,11 +6,10 @@ import './styles/base.scss'
 import './styles/footer.scss'
 import './styles/form.scss'
 import './styles/header.scss' 
+// add event listener to it when the click to call handleSubmit function
 export {
     handleSubmit,
     checkUrl
 }
 alert("I EXIST")
 console.log("CHANGE_New!!");
-//  get the button for submit
-// add event listener to it when the click to call handleSubmit function
